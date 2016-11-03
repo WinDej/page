1a117ef1012e9099e26d6b147e91c2c0eff70f4d
Paweł Żochowski <zochowski98v2@gmail.com>
2016-11-03 17:15:06 +0100

    Some formatting fixes

--- scripts/git.js
+++ scripts/git.js
@@ -1,36 +1,37 @@
 jQuery.githubUser = function(username, callback) {
-   jQuery.getJSON('https://api.github.com/users/'+username+'/repos?callback=?',callback)
+    jQuery.getJSON('https://api.github.com/users/' + username + '/repos?callback=?', callback)
 }
- 
+
 jQuery.fn.loadRepositories = function(username) {
-    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
-    
-    
+    this.html("<span>Querying GitHub for " + username + "'s repositories...</span>");
+
+
 
     var target = this;
     $.githubUser(username, function(data) {
         var repos = data.data; // JSON Parsing
-        sortByName(repos);    
-     
+        sortByName(repos);
+
         var list = $('<div class="list"/>');
         target.empty().append(list);
         $(repos).each(function() {
-            if (this.name != (username.toLowerCase()+'.github.com')) {
+            if (this.name != (username.toLowerCase() + '.github.com')) {
                 var time = fixTime(this.updated_at);
-                list.append('<div class="name"><b><a href="'+ (this.homepage?this.homepage:this.html_url) +'">' + this.name + '</a></b></div>');
-                list.append('<div class="language"><em>'+(this.language?('('+this.language+')'):'')+'</em></div>');
-                list.append('<div class="description">' + this.description +'</div>');
-                list.append('<div class="update">' + time +'</div><div class="cls"></div>');
+                list.append('<div class="name"><b><a href="' + (this.homepage ? this.homepage : this.html_url) + '">' + this.name + '</a></b></div>');
+                list.append('<div class="language"><em>' + (this.language ? ('(' + this.language + ')') : '') + '</em></div>');
+                list.append('<div class="description">' + this.description + '</div>');
+                list.append('<div class="update">' + time + '</div><div class="cls"></div>');
             }
-        });      
-      });
+        });
+    });
+
     function fixTime(t) {
         return t.slice(0, -10);
     }
-    
+
     function sortByName(repos) {
-        repos.sort(function(a,b) {
-        return a.name - b.name;
-       });
+        repos.sort(function(a, b) {
+            return a.name - b.name;
+        });
     }
 };
